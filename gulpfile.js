a6be71034676245ffdbc2f4f81e631741f9a08cd
Hanzla habib <hanzla.habib@regits.com>
2019-12-11 22:51:15 +0500

    Added Hot Reload, Image Compressor, Html error fixed,

--- gulpfile.js
+++ gulpfile.js
@@ -10,27 +10,47 @@ const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
 const browserSync = require('browser-sync').create();
+const imagemin = require('gulp-imagemin');
 const babel = require('gulp-babel')
 const webpack = require('webpack-stream')
+
 
 // File path variables
 const files = {
   scssPath: 'src/scss/**/*.scss',
   scssMainPath: 'src/scss/main.scss',
-  jsPath: 'src/js/**/*.js'
+  jsPath: 'src/js/**/*.js',
+  htmlIndexPath: 'src/index.html',
+  htmlPagesPath: 'src/pages/**/*.html',
+  htmlPath: 'src/**/*.html',
+  imagePath: 'src/images/*'
 }
+// BrowserSync Hot Reloading
 
+watch("./src/**/*.html").on('change', browserSync.reload);
 
 // Sass task
 
-function scssTask(){
-  return src(files.scssMainPath)
-        .pipe(postcss([tailwindcss(), autoprefixer(), cssnano()]))
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(sourcemaps.write('.'))
-        .pipe(dest('build/css'))
-      }
+function scssTask(done) {
+  src(files.scssMainPath)
+    .pipe(postcss([tailwindcss(), autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('build/css'))
+    .pipe(browserSync.stream())
+  done()
+}
+
+
+// Image Optimizer
+
+function imageOpt(done) {
+  src('src/images/*')
+    .pipe(imagemin())
+    .pipe(dest('build/images'));
+  done()
+}
 
 
 // JS task
@@ -41,25 +61,38 @@ function jsTask(done) {
       presets: ['@babel/env']
     }))
     .pipe(webpack())
-          .pipe(concat('main.js'))
+    .pipe(concat('main.js'))
     .pipe(dest('build/js'));
   done()
 }
-          );
+function htmlIndexTask(done) {
+  src(files.htmlIndexPath, { allowEmpty: true })
+    .pipe(dest('build/', { allowEmpty: true })
+    );
+  done();
+
+}
+
+function htmlPagesTask(done) {
+  src(files.htmlPagesPath, { allowEmpty: true })
+    .pipe(dest('./build', { allowEmpty: true })
+    );
+  done();
 }
 
 // BrowserSync method
 
-function browserSyncTask(){
+function browserSyncTask(done) {
   browserSync.init({
     server: {
-        baseDir: "./build"
+      baseDir: "./build/"
     }
-});
+  });
+  done()
 }
 //Reload method
 
-function reload(done){
+function reload(done) {
   browserSync.reload();
   done();
 }
@@ -67,22 +100,29 @@ function reload(done){
 
 // Cachebusting task
 const cbString = new Date().getTime();
-function cacheBustTask(){
-  return src(['build/index.html'])
-        .pipe(replace(/cb=\d+/g, 'cb='+cbString))
-        .pipe(dest('.'))
+function cacheBustTask(done) {
+  src(['build/index.html'], { allowEmpty: true })
+    .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
+    .pipe(dest('.'))
+  done()
 }
 // watch task
 
-function watchTask(){
-  watch([files.scssPath, files.jsPath],
-    parallel(scssTask,jsTask));
+function watchTask(done) {
+  watch([files.scssPath, files.jsPath, files.htmlPagesPath, files.htmlIndexPath],
+    parallel(scssTask, jsTask, htmlPagesTask, htmlIndexTask));
+  done()
 }
 
 // Default task
 
-exports.default = series(
-  parallel(scssTask, jsTask),
+exports.dev = series(
+  parallel(htmlPagesTask, htmlIndexTask, scssTask, jsTask, browserSyncTask),
   cacheBustTask,
   watchTask
+)
+
+exports.build = series(
+  parallel(htmlPagesTask, scssTask, jsTask, imageOpt),
+  cacheBustTask,
 )
